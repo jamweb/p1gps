41e533ae2c498e3054c1871f6d6b0250ebe592f6
jamweb <joseangel.email@gmail.com>
2018-10-02 13:48:28 +0200

    Erratas solucionadas para dockerizar

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -3,7 +3,7 @@
 #no lanzar el .jar hasta que no esté accesible el servicio de MongoDB
 #el host ‘mongo’ tendrá una IP otorgada al lanzar la pila de #servicios, y le damos el apodo ‘mongo’, utilizado aquí y en el #archivo application.properties
 
-While ! curl http://mongo:27017/
+while ! curl http://mongo:27017/
 
 do
 
